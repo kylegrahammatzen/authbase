15bf2e719db273c57dfc84533201eb653204998a
Kyle <kgrahammatzen@gmail.com>
2024-03-13 15:02:23 -0400

    Implemented getAccount method into protected page to get session information

--- app/(private)/protected/page.tsx
+++ app/(private)/protected/page.tsx
@@ -1,6 +1,12 @@
+import { getAccount } from "@/app/actions/auth/auth";
 import { LogoutLink } from "@/app/components/LogoutLink";
 
-export default function ProtectedPage() {
+export default async function ProtectedPage() {
+  const account = await getAccount();
+
+  console.log("Account:");
+  console.log(account);
+
   return (
     <>
       <div>Protected Page</div>
